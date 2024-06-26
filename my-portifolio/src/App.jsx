caac40e40f54cbfcf75330e31f8ab2ed15c577a7
André Luiz <dantasluizandre@gmail.com>
2024-06-26 15:39:56 -0300

    about me finalizado

---
+++ my-portifolio/src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./pages/Home/Home"
+
+
+import "./assets/variaveis/variavel.css"
+
+
+
+const App = () => {
+  
+
+  return (
+    <>
+     
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ my-portifolio/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// src/pages/Home/Home.jsx
+
+
+import Hero from "./sections/Hero/Hero.jsx"; // Importe corretamente o componente Hero
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Hero />
+      {/* Outros componentes ou conteúdo da página */}
+    </div>
+  );
+};
+
+export default Home;
