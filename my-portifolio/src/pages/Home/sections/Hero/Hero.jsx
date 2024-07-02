f8eb0a48288d4247aa03e45b1cff883c954c60db
André Luiz <dantasluizandre@gmail.com>
2024-07-01 21:30:33 -0300

    Serviços concluido

--- my-portifolio/src/pages/Home/sections/Hero/Hero.jsx
+++ my-portifolio/src/pages/Home/sections/Hero/Hero.jsx
@@ -5,7 +5,9 @@ import Foto from "../../../../assets/images/fotozona.png";
 import Logo from "../../../../assets/images/logo.png";
 import initTyped from "./Hero.js"; // Importar a função initTyped
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-
+import undrawWeb from "../../../../assets/images/undrawWeb.svg"
+import undrawSite from "../../../../assets/images/undrawSite.svg"
+import undrawDesign from "../../../../assets/images/undrawDesign.svg"
 const Hero = () => {
   useEffect(() => {
     const typed = initTyped(); // Inicializa o Typed.js através da função exportada
@@ -98,43 +100,47 @@ const Hero = () => {
         </div>
       </section>
 
+
+
+
+      <div className="Titulo-servico">
+      <h5>Serviços</h5>
+      </div>
+      
       <section className="servicos">
+  <div className="cards">
+    <div className="card">
+      <img src={undrawWeb} alt="Imagem 1"/>
+      <div className="card-content">
+        <h3>Desenvolvimento de websites.</h3>
+        <p>- Criação de websites modernos e responsivos seguindo o padrão de escolha do cliente</p>
+      </div>
+      <button>Saiba mais</button>
+    </div>
+    <div className="card">
+      <img src={undrawDesign} alt="Imagem 2"/>
+      <div className="card-content">
+        <h3>UI/UX.</h3>
+        <p>- Prototipagem Interativa</p>
+        <p>- Arquitetura de Software</p>
+        <p>- Design Interface</p>
+        <p>- Documentação de Software</p>
+      </div>
+      <button>Saiba mais</button>
+    </div>
+    <div className="card">
+      <img src={undrawSite} alt="Imagem 3"/>
+      <div className="card-content">
+        <h3>Manutenção de sites.</h3>
+        <p>- Serviços de manutenção para garantir que seu site esteja sempre atualizado, seguro e funcionando corretamente, incluindo correções de bugs e otimização de desempenho.</p>
+      </div>
+      <button>Saiba mais</button>
+    </div>
+  </div>
+</section>
 
-        <div className="card">
-          <div className="conteudo">
-            <h4>Desenvolvimento de websites</h4>
-            <span>
-              Criação de websites modernos e responsivos seguindo o padrão de
-              escolha do cliente
-            </span>
-          </div>
-        </div>
 
-        <div className="card">
-          <div className="conteudo">
 
-            <h4>UI/UX</h4>
-            <span>
-               Prototipagem Interativa 
-            </span>
-            <span>Arquitetura
-            de Software</span>
-            <span>Design Interface</span>
-            <span> Documentação de Software.</span>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="conteudo">
-            <h4>Manutenção de sites</h4>
-            <span>
-              Serviços de manutenção para garantir que seu site esteja sempre
-              atualizado, seguro e funcionando corretamente, incluindo correções
-              de bugs e otimização de desempenho.
-            </span>
-          </div>
-        </div>
-      </section>
     </>
   );
 };
