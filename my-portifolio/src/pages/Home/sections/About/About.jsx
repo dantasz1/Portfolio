807a50df01538b08d2629e28d90c69c19fc86dae
André Luiz <dantasluizandre@gmail.com>
2024-07-09 19:41:19 -0300

    Melhorando meu Sobre mim

--- my-portifolio/src/pages/Home/sections/About/About.jsx
+++ my-portifolio/src/pages/Home/sections/About/About.jsx
@@ -5,30 +5,31 @@ import  "../About/About.css"
 
 const About = () => {
     return (
-<section className="about-me container">
+<section id="Andre">
+  <div className="info">
+    <h2>Sobre mim</h2>
+    <span>Conheça mais sobre mim</span>
+    <hr />
+  </div>
 
-        <div className="about">
+  <div className="textoEFoto">
+    <div className="texto">
+      <p>
+         <strong>Olá, meu nome é André!</strong> Estou cursando Análise e Desenvolvimento de
+        Sistemas na universidade da UNIP Paulista, em Santos. Meu contato
+        inicial com a área de TI começou em 2023, e desde então estou
+        sempre disposto a descobrir e aprender mais, buscando
+        constantemente novos conhecimentos. Trabalho como desenvolvedor <strong> front-end e tenho experiência com HTML, CSS, JavaScript, React,
+        MongoDB e PostgreSQL . . .</strong>
+       
+      </p>
+    </div>
+    <div className="minhaFoto">
+      <img src={Foto} alt="Foto de André" />
+    </div>
+  </div>
+</section>
 
-          <img src={Foto} alt="minhaFoto" />
-
-          <div className="info">
-
-            <h2 className="aboutMe">About Me</h2>
-            
-            <p>
-              Olá, meu nome é André! Estou cursando Análise e Desenvolvimento de
-              Sistemas na universidade da UNIP Paulista, em Santos. Meu contato
-              inicial com a área de TI começou em 2023, e desde então estou
-              sempre disposto a descobrir e aprender mais, buscando
-              constantemente novos conhecimentos. Trabalho como desenvolvedor
-              front-end e tenho experiência com HTML, CSS, JavaScript, React,
-              MongoDB e PostgreSQL . . .
-            </p>
-            <button className="contate-btn">Mais Informações</button>
-            
-          </div>
-        </div>
-        </section>
     )
 }
 
--- my-portifolio/src/pages/Home/sections/Contato/Contato.jsx
+++ my-portifolio/src/pages/Home/sections/Contato/Contato.jsx
@@ -7,18 +7,24 @@ function Contato() {
   const [messagem, setMensagem] = useState("");
 
   return(
+
 <section className="contato">
 <div className="textosContato">
-<h2>Precisa de um Desenvolvedor Júnior?</h2>
+<h2>Precisa de um <span id="textoDev">Desenvolvedor Júnior?</span></h2>
 <h3>Vamos trabalhar juntos!</h3>
         </div>
  <form className="form">
 <input className="input" 
 type="text" 
 placeholder="Digite seu nome " 
-onChange={(e) => setNome(e.target.value)} />
+onChange={(e) => setNome(e.target.value)} value={nome} />
 
+<input type="text"  
+placeholder="Digite seu Email" 
+onChange={(e) => setEmail(e.target.value)}
+ value={email}/>
 
+<input type="text" placeholder="Digite uma mensagem" onChange={(e) => setMensagem(e.target.value)} value={messagem} />
  </form>
 
 </section>
