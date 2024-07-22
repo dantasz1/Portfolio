6227cbf95c28a061270e568791767f2139f4c7db
André Luiz <dantasluizandre@gmail.com>
2024-07-21 21:59:16 -0300

    menu hamburguer

--- my-portifolio/src/pages/Home/sections/NavBar/Nav.jsx
+++ my-portifolio/src/pages/Home/sections/NavBar/Nav.jsx
@@ -1,31 +1,42 @@
-import "../Hero/Hero.css";
-import Logo from "../../../../assets/images/logo.png";
-
+import { useState } from 'react';
+import Logo from '../../../../assets/images/logo.png';
+import './Nav.css';
 
 const Nav = () => {
-return (
-<nav className="nav" id="navbar">
-    <div className="nav-logo">
-      <img src={Logo} alt="MinhaLogo" />
-    </div>
-    <ul className="nav-links">
-      <li  id= "link1"  className="link">
-        <a href="#">&lt;Inicio&gt;</a>
-      </li>
-      <li id= "link2"   className="link">
-        <a href="#">&lt;Sobre mim&gt;</a>
-      </li>
-      <li id= "link3"  className="link">
-        <a href="#">&lt;Projetos&gt;</a>
-      </li>
-      
-    </ul>
-  
-  </nav>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-)
-    
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-}
+  return (
+    <nav className="nav" id="navbar">
+      <div className="nav-logo">
+        <img src={Logo} alt="MinhaLogo" />
+      </div>
+      <ul className="nav-links">
+        <li id="link1" className="link">
+          <a href="#">&lt;Inicio&gt;</a>
+        </li>
+        <li id="link2" className="link">
+          <a href="#">&lt;Sobre mim&gt;</a>
+        </li>
+        <li id="link3" className="link">
+          <a href="#">&lt;Projetos&gt;</a>
+        </li>
+        <li id="link4" className="link">
+          <a href="#">&lt;Contato&gt;</a>
+        </li>
+      </ul>
+      <button id="hamburger" className="fa fa-bars" onClick={toggleMenu}></button>
+      <div className={`mobile-menu ${isMenuOpen ? 'open' : ''}`}>
+        <li id="link1"><a href="/">&lt;Inicio&gt;</a></li>
+        <li id="link2"><a href="/">&lt;Sobre mim&gt;</a></li>
+        <li id="link3"><a href="/">&lt;Projetos&gt;</a></li>
+        <li id="link4"><a href="/">&lt;Contato&gt;</a></li>
+      </div>
+    </nav>
+  );
+};
 
-export default Nav 
+export default Nav;
